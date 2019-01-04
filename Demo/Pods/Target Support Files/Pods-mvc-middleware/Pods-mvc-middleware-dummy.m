56b0614f32332820607a6ce35cb287a6fd173c53
zsy78191 <zcleeco@qq.com>
2019-01-04 13:38:59 +0800

    p

--- Demo/Pods/Target Support Files/Pods-mvc-middleware/Pods-mvc-middleware-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mvc_middleware : NSObject
-@end
-@implementation PodsDummy_Pods_mvc_middleware
-@end
--- Demo/Pods/Target Support Files/Pods-mvc-middlewareTests/Pods-mvc-middlewareTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mvc_middlewareTests : NSObject
-@end
-@implementation PodsDummy_Pods_mvc_middlewareTests
-@end
